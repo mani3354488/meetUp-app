d324f0e70488c28e3e0e4828069cefe09103fbe2
mani3354488 <mani3354488@gmail.com>
2021-09-29 19:30:55 +0530

    basicApp complect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Todo from "./components/Todo";
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo title="FirstCard" />
+    </div>
+  );
 }
 
 export default App;
